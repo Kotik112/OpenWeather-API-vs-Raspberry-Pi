5216e10d3a27b7c5be31dd59f87a896538acfc3c
Arman I <88910492+Kotik112@users.noreply.github.com>
2022-12-12 13:23:17 +0100

    Changes to the python script

--- raspberry-pi-script/sense-hat-script.py
+++ raspberry-pi-script/sense-hat-script.py
@@ -7,14 +7,22 @@
 # This script uses the Sense HAT to gather data from the Sense HAT's sensors and send it to the IoT Hub.
 # The data is sent to the IoT Hub as a JSON object containing the temperature, humidity and pressure.
 
-# sense HAT documentation: https://pythonhosted.org/sense-hat/api/
+# See sense HAT documentation: https://pythonhosted.org/sense-hat/api/
 from sense_hat import SenseHat
-# IoTHubDeviceClient documentation: https://learn.microsoft.com/en-us/python/api/azure-iot-device/azure.iot.device.iothubdeviceclient?view=azure-python
+# See IoTHubDeviceClient documentation: https://learn.microsoft.com/en-us/python/api/azure-iot-device/azure.iot.device.iothubdeviceclient?view=azure-python
 from azure.iot.device.aio import IoTHubDeviceClient
 # env_secrets contains the connection string to the IoT Hub. Create one according to the github README.
-import env_secrets
+from env_secrets import connection_string
+import json
 import asyncio
 import time
+
+# Specific metadata for the device for blob storage
+# 
+DEVICE_ID = "raspberry-pi-1"
+DEVICE_SENSOR = "Sense HAT"
+DEVICE_LOCATION = "Stockholm"
+MEASUREMENT_UNIT = "Metric"
 
 # Define INTERVAL (in seconds) to the time between each message sent to the IoT Hub
 INTERVAL = 180
@@ -38,7 +46,11 @@ class SenseHatManager:
             "temperature": self.get_temperature(),
             "humidity": self.get_humidity(),
             "pressure": self.get_pressure(),
-            "dt": int(time.time())
+            "dt": int(time.time()),
+            "device_id": DEVICE_ID,
+            "device_sensor": DEVICE_SENSOR,
+            "device_location": DEVICE_LOCATION,
+            "measurement_unit": MEASUREMENT_UNIT
         }
 
 
@@ -46,7 +58,7 @@ class MessagingClient:
     def __init__(self):
         self.sense_hat_manager = SenseHatManager()
         # Retrieves the IoT Hub connection string from the environment variables. See env_secrets.py
-        self.CONNECTION_STRING = os.environ.get("CONNECTION_STRING")
+        self.CONNECTION_STRING = connection_string
         self.client = IoTHubDeviceClient.create_from_connection_string(self.CONNECTION_STRING)
         asyncio.run(self.client.connect(mqtt_pipeline=True))
 
@@ -64,7 +76,9 @@ class MessagingClient:
         while True:
             data = self.sense_hat_manager.get_all()
             print(f"Sending: {data}")
-            await self.send_message(f"{{\"temperature\": {data['temperature']}, \"humidity\": {data['humidity']}, \"pressure\": {data['pressure']}, \"dt\": {data['dt']}}}")
+            # Converts the data to a JSON object
+            message = json.dumps(data)
+            await self.send_message(message)
             await asyncio.sleep(INTERVAL)
 
 
